0fe18450f60a847b1d8fca6bf04f0c0e607d05b0
“hannah6530” <hannah6530@gmail.com>
2020-03-31 16:43:04 -0400

    changes made

--- routes/users.js
+++ routes/users.js
@@ -1,71 +1,75 @@
-const {
-    Router
-} = require('express');
+const { Router } = require("express");
 
-const pool = require('../db');
+const pool = require("../db");
 
 const router = Router();
 
-router.get('/', (request, response, next) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (err, res) => {
-        if (err) return next(err);
+router.get("/", (request, response, next) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (err, res) => {
+    if (err) return next(err);
 
-        response.json(res.rows);
-    });
+    response.json(res.rows);
+  });
 });
 
+router.get("/:id", (request, response, next) => {
+  const { id } = request.params;
 
-router.get('/:id', (request, response, next) => {
-    const {
-        id
-    } = request.params;
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (err, res) => {
+    if (err) return next(err);
 
-    pool.query('SELECT * FROM users WHERE id = $1', [id], (err, res) => {
-        if (err) return next(err);
-
-        response.json(res.rows);
-    });
+    response.json(res.rows);
+  });
 });
 
-router.post('/', (request, response, next) => {
-    const {
-        first_name,
-        last_name,
-        user_name,
-        password,
-        email_address
-    } = request.body;
-    pool.query('INSERT INTO users(first_name, last_name, user_name, password, email_address) VALUES($1, $2, $3, $4, $5)',
-        [first_name, last_name, user_name, password, email_address],
-        (err, res) => {
-            if (err) return next(err);
-            response.redirect('/users');
-        }
-    );
+router.post("/", (request, response, next) => {
+  const {
+    first_name,
+    last_name,
+    user_name,
+    password,
+    email_address
+  } = request.body;
+  pool.query(
+    "INSERT INTO users(first_name, last_name, user_name, password, email_address) VALUES($1, $2, $3, $4, $5)",
+    [first_name, last_name, user_name, password, email_address],
+    (err, res) => {
+      if (err) return next(err);
+      response.redirect("/users");
+    }
+  );
 });
 
-router.put('/:id', (request, response, next) => {
-    const {
-        id
-    } = request.params;
+router.put("/:id", (request, response, next) => {
+  const { id } = request.params;
 
-    const {
-        first_name,
-        last_name,
-        user_name,
-        password,
-        email_address
-    } = request.body;
+  const {
+    first_name,
+    last_name,
+    user_name,
+    password,
+    email_address
+  } = request.body;
 
+  const keys = [first_name, last_name, user_name, password, email_address];
+
+  const fields = [];
+
+  keys.forEach(key => {
+    if (request.body[key]) fields.push(key);
+  });
+
+  fields.forEach((field, index) => {
     pool.query(
-        'UPDATE users SET first_name=($1), last_name=($2), user_name=($3), password=($4), email_address=($5) WHERE id=($6)',
-        [first_name, last_name, user_name, password, email_address, id],
-        (err, res) => {
-            if (err) return next(err);
+      "UPDATE users SET ${field}=($1) WHERE id=($2)",
+      [request.body[field], id],
+      (err, res) => {
+        if (err) return next(err);
 
-            response.redirect('/users');
-        }
-    )
+        if (index === fields.length - 1) response.redirect("/users");
+      }
+    );
+  });
 });
 
 module.exports = router;
